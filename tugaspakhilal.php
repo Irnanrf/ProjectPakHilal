3789ea1f3b59e2a4c780088f213d5cd18856a2ed
Irnanrf <43727899+Irnanrf@users.noreply.github.com>
2018-10-01 00:09:44 +0700

    Create tugaspakhilal.php

---
+++ tugaspakhilal.php
@@ -0,0 +1,44 @@
+# ProjectPakHilal
+<html>
+
+<body>
+    <?php
+    $a = 10;
+    $b = 5;
+    $c = 6;
+    $d = 3.5;
+    $e = 11;
+    $f = 13;
+    $g = 4;
+    $h = 0.94;
+
+    echo "10 ~ 5 = ";
+    echo  $a + $b - ($b/2) ;
+    echo "<br>";
+    echo "10 ~ 6 = ";
+    echo $a - $c - 0.5;
+    echo "<br>";
+    echo "10 ~ 6 ~ 3.5 = ";
+    echo $e * $c - $a - $c;
+    echo "<br>";
+    echo "11 ~ 6 ~ -6.5 = ";
+    echo $a * $a + $c;
+    echo "<br>";
+    echo "11 ~ 6 = ";
+    echo $b - $a - $b - 0.5;
+    echo "<br>";
+    echo "13 ~ 4 = ";
+    echo $a - $f - $d;
+    echo "<br>";
+    echo "13 ~ 4 = ";
+    echo $a - $f - $d + $h;
+    echo "<br>";
+    echo "12 ~ 3 = ";
+    echo $a - $f - $d + $h;
+    echo "<br>";
+    echo "11 ~ 4 ~ -5.5 =";
+    echo $a - $g;
+     ?>
+</body>
+
+</html>
